bf1bc3eebb2cce467f3006682a239adb5d684588
Andre LaFleur <cincospenguinos@gmail.com>
2021-11-10 23:16:01 -0700

    Success!

--- lib/TwitterOfBabel.rb
+++ lib/TwitterOfBabel.rb
@@ -1,8 +1,23 @@
 # frozen_string_literal: true
+require 'twitter'
 
 require_relative "TwitterOfBabel/version"
 require_relative "TwitterOfBabel/babel_tweet"
 require_relative "TwitterOfBabel/address_extractor"
 
 module TwitterOfBabel
+  class TwitterOfBabel
+    def twitter_client
+      @twitter_client ||= Twitter::REST::Client.new do |config|
+        config.consumer_key = ENV['TOB_API_KEY']
+        config.consumer_secret = ENV['TOB_SECRET']
+        config.access_token = ENV['TOB_ACCESS']
+        config.access_token_secret = ENV['TOB_ACCESS_SECRET']
+      end
+    end
+
+    def respond_to_tweets
+      twitter_client.update('This is a tweet from #TwitterOfBabel')
+    end
+  end
 end
