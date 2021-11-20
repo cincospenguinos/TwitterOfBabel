782cd28bff05d271e485d9562cc4a1f500fd39a6
Andre LaFleur <cincospenguinos@gmail.com>
2021-11-20 15:58:50 -0700

    Adjust a couple things ensuring that things work

--- app.rb
+++ app.rb
@@ -8,14 +8,18 @@ end
 
 # https://api.twitter.com/1.1/account_activity/all/prod/webhooks is our magic endpoint
 get '/webhooks/twitter' do
-  digest = OpenSSL::HMAC.digest('sha256', ENV['TOB_SECRET'], params['crc_token'])
-  response_token = Base64.encode64(digest)
+  if params['crc_token']
+    digest = OpenSSL::HMAC.digest('sha256', ENV['TOB_SECRET'], params['crc_token'])
+    response_token = Base64.encode64(digest)
 
-  {
-    response_token: "sha256=#{response_token}".chomp
-  }.to_json
+    {
+      response_token: "sha256=#{response_token}".chomp
+    }.to_json
+  else
+    {}.to_json
+  end
 end
 
-post '/twitter-event' do
+post '/webhooks/twitter' do
   puts params.inspect
 end
